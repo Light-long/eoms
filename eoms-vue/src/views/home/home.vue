a2a134dd0e49ca8296ce367816f458b51326d819
Shilong Li <lishilong1108@foxmail.com>
2022-03-24 14:49:42 +0800

    完成home前端

--- eoms-vue/src/views/home/home.vue
+++ eoms-vue/src/views/home/home.vue
@@ -1,18 +1,26 @@
 <template>
 	<div class="app-container home">
+		<!--banner-->
+		<el-carousel indicator-position="outside">
+			<el-carousel-item>
+				<img src="../../assets/home/banner.jpg">
+			</el-carousel-item>
+		</el-carousel>
+		<el-divider/>
 		<el-row :gutter="20">
-			<el-col>
+			<!--公告栏-->
+			<el-col :span="10" :xs="24">
 				<el-card class="update-log">
 					<div class="demo-collapse">
-						<h2 align="center" style="font-size:30px; font-weight: bold; color: rgb(61, 82, 102)">公告栏</h2>
+						<h2 align="center" style="font-size:22px; font-weight: bold; color: rgb(61, 82, 102)">公告栏</h2>
 						<el-collapse accordion>
 							<el-collapse-item :name="index" v-for="(notice,index) in dataList">
 								<template #title>
-									<span style="font-size: 20px; font-weight: bold; color: rgb(61, 82, 102)">{{notice.title}}</span>
-									<span style="font-size: 15px; font-weight: bold; color: rgb(61, 82, 102); margin-left: 10px">{{notice.updateTime}}</span>
+									<span style="font-size: 16px; font-weight: bold; color: rgb(61, 82, 102)">{{notice.title}}</span>
+									<span style="font-size: 14px; font-weight: bold; color: rgb(61, 82, 102); margin-left: 10px">{{notice.updateTime}}</span>
 									<el-tag class="ml-2" size="small" type="success" style="margin-left: 20px" v-if="notice.isTopping === 1">置顶</el-tag>
 								</template>
-								<div style="font-size: 15px; font-weight: bold;">
+								<div style="font-size: 14px; font-weight: normal;">
 									{{notice.content}}
 								</div>
 							</el-collapse-item>
@@ -20,8 +28,36 @@
 					</div>
 				</el-card>
 			</el-col>
+			<!--公司发展历程-->
+			<el-col :span="14" :xs="24">
+				<el-card class="update-log">
+					<div class="demo-collapse">
+						<h2 align="center" style="font-size: 22px; font-weight: bold; color: rgb(61, 82, 102)">公司发展历程</h2>
+					</div>
+					<hr>
+					<el-timeline>
+						<el-timeline-item timestamp="2022/2/12" placement="top" :type="'info'">
+							<el-card>
+								<h4>完成天使轮融资</h4>
+								<p>2022年2月12日，本公司完成了由tx领投的300w的天使轮融资</p>
+							</el-card>
+						</el-timeline-item>
+						<el-timeline-item timestamp="2022/3/12" placement="top" :type="'primary'">
+							<el-card>
+								<h4>完成A轮融资</h4>
+								<p>2022年3月12日，本公司完成了由tx领投的800w的A轮融资</p>
+							</el-card>
+						</el-timeline-item>
+						<el-timeline-item timestamp="2022/4/12" placement="top" :type="'success'">
+							<el-card>
+								<h4>完成B轮融资</h4>
+								<p>2022年4月12日，本公司完成了由tx领投的2000w的B轮融资</p>
+							</el-card>
+						</el-timeline-item>
+					</el-timeline>
+				</el-card>
+			</el-col>
 		</el-row>
-		<el-divider />
 	</div>
 </template>
 
